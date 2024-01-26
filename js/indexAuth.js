915744bf480a5d9bc510050ff0e3f57864c390fc
João Pedro <joaopedromouramendes1@gmail.com>
2024-01-26 18:08:57 -0300

    Ajustes

--- js/indexAuth.js
+++ js/indexAuth.js
@@ -7,6 +7,6 @@ async function init() {
     // Verifica se o usuário está logado
     const status = await logged();
     if (!status) {
-        location.assign(`http://${location.hostname}:5500/pages/login.html`);
+        location.href = `${location.hostname}/pages/login.html`;
     }
 }
